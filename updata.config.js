b6ec40041a230b3991d35fe190d5f35ef8a34694
FliPPeDround <734243792@qq.com>
2022-01-13 22:08:06 +0800

    feat: 添加版本号自动更新脚本

---
+++ updata.config.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+
+const data = fs.readFileSync('package.json')
+
+function replacer(key, value) {
+  if (key === "version") {
+    const versionArry = value.split('.').map(Number)
+    if (versionArry[2] + 1 > 9) {
+      versionArry[2] = 0
+      if (versionArry[1] + 1 > 9) {
+        versionArry[1] = 0
+        versionArry[0] += 1
+      } else {
+        versionArry[1] += 1
+      }
+    } else {
+      versionArry[2] += 1
+    }
+    const version = versionArry.map(String).join('.')
+    console.log('version: '+ version)
+    return version
+  }
+  return value;
+}
+
+const newPackage = JSON.stringify(JSON.parse(data),replacer,2)
+fs.writeFileSync('package.json', newPackage, 'utf8')
