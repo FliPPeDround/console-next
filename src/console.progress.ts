b98ce9a0b171167b2993381735d23fe9ef30992d
legend80s <liuchzong@qq.com>
2022-04-23 23:17:33 +0800

    style: format code

--- src/console.progress.ts
+++ src/console.progress.ts
@@ -1,22 +1,26 @@
 function progressStyle (percentage: number, progressInterval: NodeJS.Timer) {
   const stroke = '█'
-    const unstoke = '░'
-    if (percentage >= 100) {
-      console.clear()
-      console.log(`${stroke.repeat(40)} 100%`)
-      console.color([{
-        color: 'rgb(0,255,0)',
-        content: '❤️ success loaded'
-      }])
-      clearInterval(progressInterval)
-      return
-    }
-    const strokeWidth = Math.round(percentage/100*40)
-    const unstrokeWidth = 40 - strokeWidth
-    console.clear()
-    console.log(`${stroke.repeat(strokeWidth)}${unstoke.repeat(unstrokeWidth)} ${percentage}%`)
-}
+  const unstoke = '░'
 
+  if (percentage >= 100) {
+    console.clear()
+    console.log(`${stroke.repeat(40)} 100%`)
+    console.color([{
+      color: 'rgb(0,255,0)',
+      content: '❤️ success loaded'
+    }])
+
+    clearInterval(progressInterval)
+
+    return
+  }
+
+  const strokeWidth = Math.round(percentage/100*40)
+  const unstrokeWidth = 40 - strokeWidth
+
+  console.clear()
+  console.log(`${stroke.repeat(strokeWidth)}${unstoke.repeat(unstrokeWidth)} ${percentage}%`)
+}
 
 Object.defineProperty(console, "progress", {
   value: function progress (percentage: number, callback: () => number, ms: number) {
@@ -24,7 +28,7 @@ Object.defineProperty(console, "progress", {
       progressStyle(percentage, progressInterval)
       percentage = callback()
     }, ms)
-    
   },
+
   enumerable: true,
 })
